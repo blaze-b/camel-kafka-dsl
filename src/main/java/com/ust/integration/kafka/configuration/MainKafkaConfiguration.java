b9e4aeb1294703e09dd0e9b89efb150d8e8e3bdb
Brian Blaze <55305942+brianblaze14@users.noreply.github.com>
2019-11-11 16:22:34 +0530

    Update MainKafkaConfiguration.java

--- src/main/java/com/ust/integration/kafka/configuration/MainKafkaConfiguration.java
+++ src/main/java/com/ust/integration/kafka/configuration/MainKafkaConfiguration.java
@@ -2,29 +2,41 @@ package com.ust.integration.kafka.configuration;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.ThreadPoolRejectedPolicy;
+import org.apache.camel.component.kafka.KafkaComponent;
+import org.apache.camel.component.kafka.KafkaConfiguration;
 import org.apache.camel.spi.ThreadPoolProfile;
 import org.apache.camel.spring.boot.CamelContextConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MainKafkaConfiguration {
 
-    @Bean
-    CamelContextConfiguration contextConfiguration() {
-        return new CamelContextConfiguration() {
-            public void beforeApplicationStart(CamelContext context) {
-                // your custom configuration goes here
-                ThreadPoolProfile threadPoolProfile = new ThreadPoolProfile();
-                threadPoolProfile.setId("MyDefault");
-                threadPoolProfile.setPoolSize(10);
-                threadPoolProfile.setMaxPoolSize(15);
-                threadPoolProfile.setMaxQueueSize(250);
-                threadPoolProfile.setKeepAliveTime(25L);
-                threadPoolProfile.setRejectedPolicy(ThreadPoolRejectedPolicy.Abort);
-                context.getExecutorServiceManager().registerThreadPoolProfile(threadPoolProfile);
+	public static final String MY_DEFAULT_PROFILE = "myDefaultKafkaProfile";
+	public static final String KAFKA = "kafka";
 
-            }
-        };
-    }
+	@Bean
+	CamelContextConfiguration contextConfiguration(@Value("${kafka.host}") final String host,
+			@Value("${kafka.port}") final String port, @Value("${kafka.channel}") final String groupID) {
+		return new CamelContextConfiguration() {
+			public void beforeApplicationStart(CamelContext camelContext) {
+				// your custom configuration goes here
+				ThreadPoolProfile threadPoolProfile = new ThreadPoolProfile();
+				threadPoolProfile.setId(MY_DEFAULT_PROFILE);
+				threadPoolProfile.setPoolSize(10);
+				threadPoolProfile.setMaxPoolSize(15);
+				threadPoolProfile.setMaxQueueSize(250);
+				threadPoolProfile.setKeepAliveTime(25L);
+				threadPoolProfile.setRejectedPolicy(ThreadPoolRejectedPolicy.Abort);
+				camelContext.getExecutorServiceManager().registerThreadPoolProfile(threadPoolProfile);
+				KafkaComponent kafkaComponent = new KafkaComponent();
+				KafkaConfiguration kafkaConfig = new KafkaConfiguration();
+				kafkaConfig.setBrokers(host + ":" + port);
+				kafkaConfig.setGroupId(groupID);
+				kafkaComponent.setConfiguration(kafkaConfig);
+				camelContext.addComponent(KAFKA, kafkaComponent);
+			}
+		};
+	}
 }
